1c488ffc0f71e449ddb87ae2de446e0d02875738
dir-FY24onoyuto <yuto.ono.t380@dirbato.co.jp>
2024-04-25 11:47:10 +0900

    chapter4

---
+++ TutorialDevelop/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO user(name, gender, age, email)
+        VALUES ("キラメキ太郎", "男性", 27, 'taro.kiramkei@mail.com');
+INSERT INTO user(name, gender, age, email)
+        VALUES ("キラメキ次郎", "男性", 22, 'jiro.kiramkei@mail.com');
+INSERT INTO user(name, gender, age, email)
+        VALUES ("キラメキ花子", "女性", 25, 'hanako.kiramkei@mail.com');
+        
+        
